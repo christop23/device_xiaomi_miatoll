fa2df55410747d9bd9a48b61fa7f5b539133512d
Christo Paul <christop23@proton.me>
2024-09-03 21:54:05 +0530

    Sakura changes

--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit some common Genesis stuff.
-$(call inherit-product, vendor/ultimate/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/miatoll.mk)
@@ -18,17 +18,13 @@ $(call inherit-product, device/xiaomi/miatoll/miatoll.mk)
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# GenesisOS stuff.
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_AOSP_RECOVERY := true
-USE_PIXEL_CHARGER := true
+# Sakura stuff
+SAKURA_OFFICIAL := true
+SAKURA_MAINTAINER := orionphoenix
+TARGET_BOOT_ANIMATION_RES := 1080
+SAKURA_BUILD_TYPE := vanilla
 
-ULTIMATE_OFFICIAL := true
-
-PRODUCT_NAME := ultimate_miatoll
+PRODUCT_NAME := lineage_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
